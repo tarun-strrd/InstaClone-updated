9c58746ab8359ec6abdc666b5be8876058f0d079
tarun-strrd <tarunsayyapureddi123@gmail.com>
2023-07-14 00:03:20 +0530

    vercel changes

--- app.js
+++ app.js
@@ -30,13 +30,12 @@ app.get("/", (req, res) => {
   res.send("hello world");
 });
 
-if (process.env.NODE_ENV == "production") {
-  app.use(express.static("client/build"));
-  const path = require("path");
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
 });
